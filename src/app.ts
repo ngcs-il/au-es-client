de0478b21dde77861636aaaa6754c69f7ba12e7e
David Kossoglyad <david@logoui.co.il>
2021-04-04 03:46:05 +0300

    feat: implement navigation

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
 import { PLATFORM } from 'aurelia-pal';
-import { RouterConfiguration, Router, activationStrategy } from 'aurelia-router';
+import { RouterConfiguration, Router } from 'aurelia-router';
   
   export class App {
     router: Router;
   
     configureRouter(config: RouterConfiguration, router: Router): void {
       this.router = router;
-      config.title = 'Aurelia';
+      config.title = 'NGCS Search';
       config.options.pushState = true;
       config.options.root = '/';
       config.options.compareQueryParams = true;
       config.map([
-        { route: ['', 'search/:q?'], name: 'search', moduleId: PLATFORM.moduleName('search'), activationStrategy: activationStrategy.replace }
+        { route: ['', 'search'], name: 'search', moduleId: PLATFORM.moduleName('search')},
+        { route: ['', 'search*'], name: 'search', moduleId: PLATFORM.moduleName('search')}
       ]);
     }
     
--- src/search.ts
+++ src/search.ts
@@ -1,35 +1,61 @@
-import { SearchResultDto, SearchResultItemFragmentDto } from './data/data-provider';
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NavigationInstruction, RouteConfig } from 'aurelia-router';
+import { singleton } from 'aurelia-framework';
+import { SearchResultDto, SearchResultItemFragmentDto } from './data/data-provider';
+import { NavigationInstruction, RouteConfig, Router } from 'aurelia-router';
 import { DataProvider } from 'data';
 
 class Query {
   q = '';
-  withCherry;
-  withApple;
+  withCherry = false;
+  withApple = false;
 }
 
+@singleton(Search)
 export class Search {
   
   model: Query = new Query() ;
   displayModel: any = {};
 
   data: SearchResultDto;
+  static counter = 0;
+  constructor(private router: Router) {}
 
-  private async activate(params: any, _: RouteConfig, navigationInstruction: NavigationInstruction): Promise<void> {
+  search() {
+    if (!this.model.q) {
+      return;
+    }
+    let fragment = `search/?q=${this.model.q}`;
+    if (this.model.withApple) {
+      fragment += `&withApple=${this.model.withApple}`;
+    }
+    if (this.model.withCherry) {
+      fragment += `&withCherry=${this.model.withCherry}`;
+    }
+    this.router.navigate(fragment, { replace: false, trigger: true });
+  }
+
+  private async activate(params: any, config: RouteConfig, navigationInstruction: NavigationInstruction): Promise<void> {
+    console.log(`Counter: ${Search.counter++}`);
+    console.log(JSON.stringify(navigationInstruction.queryParams))
     this.displayModel.searchFor = JSON.stringify(params);
     this.displayModel.router = JSON.stringify(navigationInstruction.queryParams);
     
-    setTimeout(()=> {
-      this.model.q = navigationInstruction.queryParams?.q;
-      this.model.withApple =  (navigationInstruction.queryParams?.withApple === 'on'? true:false); 
-      this.model.withCherry = (navigationInstruction.queryParams?.withCherry === 'on'? true:false);
-    }, 500);
+   
     
     if (navigationInstruction.queryParams?.q?.length > 0) {
       const dataProvider: DataProvider = new DataProvider();
       this.data = await dataProvider.getSearchResults(navigationInstruction.queryParams.q, 10, 1);
+    } else {
+      this.data = undefined;
+    }
+
+    this.model.q = navigationInstruction.queryParams?.q;
+    this.model.withApple =  (navigationInstruction.queryParams?.withApple ? true:false); 
+    this.model.withCherry = (navigationInstruction.queryParams?.withCherry ? true:false);
+ 
+    if (navigationInstruction.queryParams?.q) {
+      this.router.title = navigationInstruction.queryParams.q;
     }
   }
 
